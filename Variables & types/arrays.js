b0c72c0140f3f3c08797dba08d4554fed9640e6b
mohammed taha aouari <mouhammedtahaaouari1@gmail.com>
2026-09-21 21:35:50 +0100

    Add files via upload

---
+++ Variables & types/arrays.js
@@ -0,0 +1,6 @@
+let names = ["taha" , "negga" , "garwex" , "ilyass" , "herlo"]
+for (let i = 0; i < names.length; i++) {
+    if (!names[i].includes("a")) {
+        console.log(names[i]) ;
+    }
+}
