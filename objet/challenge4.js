e3b6c43ab3856360280040f4cfb71f1fc4e2b3fa
mohammed taha aouari <mouhammedtahaaouari1@gmail.com>
2026-09-22 18:13:19 +0100

    Add files via upload

---
+++ objet/challenge4.js
@@ -0,0 +1,9 @@
+const point = {
+    x: 5,
+    y: 8,
+}
+        console.log("point est x = " + point.x + "point y est =" + point.y);
+
+        point.x = 10;
+        point.y = 12;
+        console.log(`nouvelle point est x = ${point.x} y = ${point.y}`)
