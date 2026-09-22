eb5a1cbaad030077b6d42a9446ae8e3e61824c02
mohammed taha aouari <mouhammedtahaaouari1@gmail.com>
2026-09-22 17:34:39 +0100

    Enhance student object with notes and average calculation
    Added calculation of average and notes display for student.

--- objet/challenge2.js
+++ objet/challenge2.js
@@ -3,5 +3,11 @@ let etudiant = {
     prenom: "jokh",
     notes: [20 , 12 , 02 , 10]
     };
-    console.log(`etudiant : ${etudiant.prenom} ${etudiant.nom}`);
-    
+    console.log(`${etudiant.prenom} ${etudiant.nom}`);
+    console.log("notes: " + etudiant.notes.join(", "));
+    let total = 0;
+for (let note of etudiant.notes) {
+    total += note;
+}
+let a = total / etudiant.notes.length;
+console.log(`Moyenne : ${a}`);
